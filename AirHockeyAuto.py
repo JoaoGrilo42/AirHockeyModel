7388ccb549ecac7dfe60c5d4e00b1f1d44121b5c
JoaoGrilo <larryballard@centurylink.net>
2020-08-28 23:55:40 -0600

    Modified Collision to a single function, and updated documentation

--- AirHockeyAuto.py
+++ AirHockeyAuto.py
@@ -11,7 +11,7 @@ size = width, height = int(1219/mm2pixel), int(2134/mm2pixel) #size of table in 
 
 screen = pg.display.set_mode(size)
 
-dt = .01 # update interval in seconds
+dt = .005 # update interval in seconds
 
 class Puck :
     def __init__(self):
@@ -20,28 +20,26 @@ class Puck :
         self.puck = pg.draw.circle(screen, pg.Color('red'), np.multiply(1/mm2pixel, self.pos).astype(int), int(self.radius/mm2pixel) ,2)
         self.m = 42 # grams
         self.v = np.array([0,0]) #mm/s
-        #self.theta = 0 # Direction of velocity (0 = right)
-        self.mu_F = 10 # Coefficient of friction of puck on table
+        self.mu_F = 5 # Coefficient of friction of puck on table
         self.g = 9.8 # gravity m/s^2
         self.dt_impact = .5# time duration of impact
 
 
-    def collisionWall(self, theta_wall) :
-        print("Wall Collision")
-        self.v = self.v - 2*np.abs(self.v)*[np.cos(theta_wall), np.sin(theta_wall)]
-
-    def collisionArm(self, pos_arm, theta_arm, F_arm) :
-        print("Arm Collision")
-        print("Position:\nPuck: ({}, {}), Arm: ({}, {})".format(*self.pos, *pos_arm))
-        theta_n = np.arctan2(*np.array(self.pos - pos_arm)[::-1])
+    def collision(self, pos, theta, F) :
+        print("Collision")
+        print("Position:\nPuck: ({}, {}), Object: ({}, {})".format(*self.pos, *pos))
+        theta_n = np.arctan2(*np.array(self.pos - pos)[::-1])
         theta_puck = np.arctan2(*self.v[::-1])
-        print("Theta_n: {}, Theta_puck: {}, Theta_arm: {}".format(theta_n, theta_puck, theta_arm))
-        print("{} * {} + {} * {}".format(np.linalg.norm(self.v), np.cos(theta_puck - theta_n),  dt/self.m*F_arm, np.cos(theta_arm-theta_n)))
-        V_n = (np.linalg.norm(self.v) * np.cos(theta_puck - theta_n))+(self.dt_impact / self.m * F_arm * np.cos(theta_arm-theta_n))        
+        print("Theta_n: {}, Theta_puck: {}, Theta: {}".format(theta_n, theta_puck, theta))
+        V_n = (np.linalg.norm(self.v) * np.cos(theta_puck - theta_n))+(self.dt_impact * F / self.m * np.cos(theta-theta_n))        
         V_p = (np.linalg.norm(self.v) * np.sin(theta_puck - theta_n))
-        print("{} * [{}, {}] + [{}, {}]".format(V_n, *np.array([np.cos(theta_n), np.sin(theta_n)]), *self.v))
-        self.v = V_n*np.array([np.cos(theta_n), np.sin(theta_n)]) + V_p*np.array([np.cos(math.pi/2 + theta_n), np.sin(math.pi/2 + theta_n)])
-
+        print("V_n: {} = {} * {} + {} * {}".format(V_n, np.linalg.norm(self.v),np.cos(theta_puck - theta_n), self.dt_impact / self.m * F, np.cos(theta-theta_n)))
+        print("V_p: {} = {} * {}".format(V_p, np.linalg.norm(self.v), np.sin(theta_puck - theta_n)))
+        print("v(t-1): {}, {}".format(self.v, np.linalg.norm(self.v)))
+        print("v(t) = {} * [{}, {}] + {} * [{}, {}]".format(V_n, *np.array([np.cos(theta_n), np.sin(theta_n)]),  V_p, *np.array([np.cos(theta_n - math.pi/2), np.sin(theta_n - math.pi/2)])))
+        self.v = V_n*np.array([np.cos(theta_n), np.sin(theta_n)]) + V_p*np.array([np.cos(theta_n + math.pi/2), np.sin(theta_n + math.pi/2)])
+        print("v(t): {}, {}".format(self.v, np.linalg.norm(self.v)))
+    
     def friction(self) :
          theta = np.arctan2(*self.v[::-1])
          print("Theta: {}".format(theta))
@@ -52,31 +50,39 @@ class Puck :
         print("pos ({}, {})".format(*self.pos))
         print("dpos ({}, {})".format(*np.multiply(dt, self.v)))
         print("v ({}, {})".format(*self.v))
+        
         screen.fill(pg.Color('black'), self.puck)
-        self.pos = self.pos + dt * self.v
-        self.puck = pg.draw.circle(screen, pg.Color('red'), np.multiply(1/mm2pixel, self.pos).astype(int), int(self.radius/mm2pixel), 2)
+        
         if self.pos[0]+self.radius > width*mm2pixel :
-            self.collisionWall(0)
+            self.pos[0] = width*mm2pixel-self.radius
+            self.collision(self.pos + [self.radius, 0], math.pi + np.arctan2(*self.v[::-1]), 2*self.m*np.linalg.norm(self.v)/self.dt_impact)
         if self.pos[0]-self.radius < 0:
-            self.collisionWall(math.pi)
+            self.pos[0] = self.radius
+            self.collision(self.pos - [-self.radius, 0], math.pi+np.arctan2(*self.v[::-1]), 2*self.m*np.linalg.norm(self.v)/self.dt_impact)
         if self.pos[1]-self.radius < 0:
-            self.collisionWall(3*math.pi/2)
+            self.pos[1] = self.radius
+            self.collision(self.pos - [0, -self.radius], math.pi + np.arctan2(*self.v[::-1]), 2*self.m*np.linalg.norm(self.v)/self.dt_impact)
         if self.pos[1]+self.radius > height*mm2pixel:
-            self.collisionWall(math.pi/2)
+            self.pos[1] = height*mm2pixel-self.radius
+            self.collision(self.pos + [0, self.radius], math.pi +np.arctan2(*self.v[::-1]), 2*self.m*np.linalg.norm(self.v)/self.dt_impact)
+        
         self.friction()
+        
+        self.pos = self.pos + dt * self.v
+        
+        self.puck = pg.draw.circle(screen, pg.Color('red'), np.multiply(1/mm2pixel, self.pos).astype(int), int(self.radius/mm2pixel), 2)
+
 gamePuck = Puck()
 
-gamePuck.v = np.array([100,-100])
-#gamePuck.theta = math.pi/4
 HITPUCK = pg.USEREVENT+1
 
-pg.time.set_timer(HITPUCK, 10000)
+pg.time.set_timer(HITPUCK, 2000)
 while 1:
     for event in pg.event.get():
         if event.type == pg.QUIT: sys.exit()
         if event.type == HITPUCK:
             print("*******Should hit puck now**********")
-            gamePuck.collisionArm(gamePuck.pos+2, -3*math.pi/4, 20000)
+            gamePuck.collision(gamePuck.pos+2, -3*math.pi/4, 200000)
 
     gamePuck.update()
     #pg.draw.circle(screen, )
--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # AirHockeyModel
+The goal of this project is to eventually design and build a robotic air hockey opponent.  The idea being that I will first develop a software model that I can use not only to prove the concept, but primarily as a platform to develop the control algorithms.  As a bonus, I will use this chance to learn python.
+
+# The Table Model
+The air hockey table model will be fairly ideal.  I will be modeling perfect elastic collisions with uniform dynamic friciton and no static friction.  
+I will be modeling the puck as an object with the following attributes
+* radius (42 mm)
+* mass (42 g)
+* Position (x, y) 
+* Velocity (dx, dy) (mm/s)
+* Theta (heading rad)
+* Coefficient of Friction (mu_F)
+* g (gravity 9.8 m/s/s)
+* dt_impact (duration of impacts)
+
+The puck will also have the following methods 
+* collsion
+* friction
+* update
+Each of these methods will be used to interact with the puck.
+## collision
+This function takes as arguments the position (x,y) of the other object, the vector angle of the force, and the magnitude of the force.  The position is used to calculate the direction of the vector normal to the tangent collision point ie the direction that force will act on the puck.  
+This function first calculates the angles that will be used, namely theta_n (the angle of the force normal to the tangent of the collision point) and theta_puch (the direction of motion of the puck).
+Next the puck velocity and colision force multiplied by dt_impact and divided by mass of puck are projected onto the theta_n vector and added together to give V_n.
+The projection of the puck velocity perpendicular to the theta_n vector is also calculated to give V_p (The force is not included in this equations because it doesn't act perpendicular to the normal vector)
+Finally V_n and V_p are projected back on the x-y plane and added together to give the new x-y velocity of the puck.
+
+There are two types of collisions that use this function.  The first is the collision with the wall.  In this case the the point of impact is calculated based on the puck position and the direction of the wall being impacted.  Theta 0, pi/2, pi, or 3pi/2, and is chosen to be in the direction to the puck from the wall.  And finaly F is selected as 2*m_puck*V/dt_impact where m_puck is the mass of the puck, V is the magnitude of the current puck velocity and dt_impact is the duration of the impact where the force is applied.  This force will result in lossless reflection.
+
+The second type of collision is with the paddle.  In this case the position of the paddle and puck will be used to determine the impact point and a control algorithm will be used to select the force direction and magnitude.
+
+## friction
+This simple function applies a frictional force in the direction opposite the velocity of the puck.  The ammount that the puck slows due to friction is calculated by multiplying the coefficient of friction with gravity and the update interval (dt) subtracting that from the current velocity.
+
+## update
+As of right now the update function is called by the main loop to update the state of the puck by applying friction and collisions with the table walls.  It also handles drawing the puck.
+
+# Player Model
